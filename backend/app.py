5102acbf7dfe4f40e05f2632328b8c8cf636e399
Hanson Qin <hansonq888@gmail.com>
2025-10-15 16:39:01 -0400

    updated backend CORS

--- backend/app.py
+++ backend/app.py
@@ -32,7 +32,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176",
-        "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:5175", "http://127.0.0.1:5176", "https://econ-dashboard-jm8u-kq9yx8qyo-hansons-projects-ae068754.vercel.app/"
+        "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:5175", "http://127.0.0.1:5176",
+        "https://econ-dashboard-jm8u-kq9yx8qyo-hansons-projects-ae068754.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
